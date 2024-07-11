32b633c39a762ba2f3b0b6e2dc0ea6bb30b2c5ac
IsaacWinner18 <winsamlab222@gmail.com>
2024-07-11 15:36:28 -0700

    making progress

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
+  const confetti = document.querySelector(".confetti");
   
   const claim_btn = document.querySelector(".claimbtn");
   const show_countdown = document.querySelector(".countdown");
 let earning_counter = document.querySelector(".earningcounter");
-let earning = 0;
-
+let earning = parseInt(localStorage.getItem('earning')) || 0;
 earning_counter.textContent = earning;
 
    // Set the countdown timer
@@ -24,6 +25,7 @@ earning_counter.textContent = earning;
      else {
       clearInterval(intervalId);
       claim_btn.style.display = "inline-block";
+      confetti.style.display = "block";
     show_countdown.style.display = "none";
 
      }
@@ -33,6 +35,7 @@ earning_counter.textContent = earning;
   
    claim_btn.addEventListener("click", ()=>{
     claim_btn.style.display = "none";
+    confetti.style.display = "none";
     show_countdown.style.display = "block";
     countdownTime = 0o1 * 1 * 2 + 0o0 * 30 + 0;
     // countdownTime = 0o1 * 60 * 60 + 0o0 * 60 + 0;
@@ -40,41 +43,9 @@ earning_counter.textContent = earning;
 
     earning += 1000;
     earning_counter.textContent = earning;
-    console.log(earning)
+    localStorage.setItem("earning", earning)
   })
   
-
-//   const claim_btn = document.querySelector(".claimbtn");
-// const show_countdown = document.querySelector(".countdown");
-
-// // Set the countdown timer
-// let countdownElement = document.getElementById('countdown');
-// let countdownTime = 4; // initial countdown time in seconds
-
-// function updateCountdown() {
-//   let minutes = Math.floor(countdownTime / 60);
-//   let seconds = countdownTime % 60;
-
-//   countdownElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-
-//   if (countdownTime > 0) {
-//     countdownTime--;
-//   } else {
-//     clearInterval(intervalId);
-//     claim_btn.style.display = "inline-block";
-//     show_countdown.style.display = "none";
-//   }
-// }
-
-// let intervalId;
-
-// claim_btn.addEventListener("click", () => {
-//   claim_btn.style.display = "none";
-//   show_countdown.style.display = "block";
-//   countdownTime = 4; // reset the countdown time
-//   intervalId = setInterval(updateCountdown, 1000);
-// });
-
 
 
   });
